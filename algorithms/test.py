1680e2522b99c89c7610836a04525ff54fc30b12
gingira <gingiravictory@outlook.com>
2020-10-13 01:47:04 +0800

    update after 806

--- algorithms/test.py
+++ algorithms/test.py
@@ -1,3 +1,12 @@
-s = ''
-if s:
-    print(1)
+def tes():
+    for i in 'python':
+        if i == 'x':
+            return 12
+        elif i == 'l':
+            return 13
+        else:
+            return len('python')
+
+print(tes())
+
+
