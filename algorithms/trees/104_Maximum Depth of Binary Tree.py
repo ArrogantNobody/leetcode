dda562f846e73f7762ac5113cd68ff1ccd900395
gingira <gingiravictory@outlook.com>
2020-10-22 13:07:20 +0800

    update 2020/10/22 13:07

---
+++ algorithms/trees/104_Maximum Depth of Binary Tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        queue = [root]
+        res = []
+        while queue:
+            layer = []
+            in_queue = []
+            for node in queue:
+                if node:
+                    layer.append(node.val)
+                if node.left:
+                    in_queue.append(node.left)
+                if node.right:
+                    in_queue.append(node.right)
+            queue = in_queue
+            res.append(layer)
+        return len(res)
+
+#recursion
+# if root is None: return 0
+# return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
